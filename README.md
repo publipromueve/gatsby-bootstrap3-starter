61ec340b798a0e7315d50a85d45b18c27a66be07
Gabriel Peña <gabrielpenac25@gmail.com>
2018-01-23 17:33:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# gatsby-bootstrap3-starter
+
+
+Install this starter (assuming Gatsby is installed) by running from your CLI:
+```
+gatsby new gatsby-site https://github.com/publipromueve/gatsby-bootstrap3-starter
+```
+
+## Running in development
+`npm run develop`
+
+## Build
+`npm run build`
+
+## Serve production version
+`npm run serve`
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+
+import theme from '../styles/theme'
+import media from '../styles/media'
+
+const Container = styled.div`
+  background-color: ${props => props.theme.mainColor};
+
+  ${media.lg`padding: 0 20px;`}
+`
+
+export default () => (
+  <ThemeProvider theme={theme}>
+    <Container className="row">Hello world!</Container>
+  </ThemeProvider>
+)
